1ef4c8f4e6c51127506daa25d741b460d3082319
Anne Porsgaard <anne@besenbacher.com>
2018-11-06 14:27:45 +0100

    Added a timer class from the Mircrowave exercise

---
+++ ATMHandin3/ATMHandin3/Interfaces/ITimer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMHandin3.Interfaces
+{
+    public interface ITimer
+    {
+        int TimeRemaining { get; }
+        event EventHandler Expired;
+        event EventHandler TimerTick;
+
+        void Start(int time);
+        void Stop();
+    }
+}
