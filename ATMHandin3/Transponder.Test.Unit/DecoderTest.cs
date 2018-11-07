3aba1cff1d9626165d7af78f12133b813c2a21d4
Anne Porsgaard <anne@besenbacher.com>
2018-11-07 13:29:42 +0100

    Changed the decoder test

--- ATMHandin3/Transponder.Test.Unit/DecoderTest.cs
+++ ATMHandin3/Transponder.Test.Unit/DecoderTest.cs
@@ -20,18 +20,8 @@ namespace Transponder.Test.Unit
             _fakeTransponderReceiver = Substitute.For<ITransponderReceiver>();
             _fakeDecoder = Substitute.For<IDecoder>();
             _uut = new Decoder(_fakeTransponderReceiver);
-        }
 
-        [Test]
-        public void TestReception()
-        {
-            List<string> testData = new List<string>();
-            testData.Add("ATR423;39045;12932;14000;20151006213456789");
-            testData.Add("BCD123;10005;85890;12000;20151006213456789");
-            testData.Add("XYZ987;25059;75654;4000;20151006213456789");
-
-            _fakeTransponderReceiver.TransponderDataReady +=
-                Raise.EventWith(this, new RawTransponderDataEventArgs(testData));
+            _fakeTransponderReceiver.TransponderDataReady += (o, args) => { };
         }
 
         [Test]
