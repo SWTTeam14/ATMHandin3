8222b439d9fc346d79a64ba79205a7f7db89ea7a
Anne Porsgaard <anne@besenbacher.com>
2018-11-07 20:32:34 +0100

    Refactored filelogger to fix bug related to non existing text file. ReceiverWarningRaised is now assumed only to be raised once (i.e. when the tracks become conflicting).  If an aircraft later raises an event, it will also be logged to the file now.

--- ATMHandin3/ATMHandin3/Classes/FileLogger.cs
+++ ATMHandin3/ATMHandin3/Classes/FileLogger.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ATMHandin3.Classes;
+﻿using System.IO;
 using ATMHandin3.Events;
 using ATMHandin3.Interfaces;
 
@@ -12,69 +6,20 @@ namespace ATMHandin3.Classes
 {
     public class FileLogger : IFileLogger
     {
-        private ICollisionAvoidanceSystem eventReceiver;
+        private ICollisionAvoidanceSystem _eventReceiver;
 
-        public FileLogger(ICollisionAvoidanceSystem _eventReceiver)
+        public FileLogger(ICollisionAvoidanceSystem eventReceiver)
         {
-            eventReceiver = _eventReceiver;
-
+            _eventReceiver = eventReceiver;
             _eventReceiver.SeparationEvent += ReceiverWarningRaised;
         }
 
         public void ReceiverWarningRaised(object sender, SeparationEventArgs e)
         {
-            for (int i = 0; i < 2; i++)
+            using (StreamWriter log = File.AppendText(@"SeparationEventLogFile.txt"))
             {
-                for (int j = i+1; j < 2; j++)
-                {
-                    var air1 = e.a1;
-                    var air2 = e.a2;
-
-                    //FileStream fs = new FileStream(@"SeparationEventLog.txt", FileMode.OpenOrCreate, FileAccess.Write);
-                    //StreamWriter sw = new StreamWriter(fs);
-                    //TextWriter tw = Console.Out;
-                    //
-                    ////TextWriter tsw = new StreamWriter(@"SeparationEventLog.txt", true);
-                    //
-                    //Console.SetOut(sw);
-                    ////Console.WriteLine("WARNING!!!! {0}, you are on a collision course with {1}. At: {2}. Divert course!",
-                    ////    air1.Tag, air2.Tag, air1.TimeStamp, Environment.NewLine);
-                    //
-                    //
-                    //sw.WriteLine("WARNING!!!! {0}, you are on a collision course with {1}. At: {2}. Divert course!",
-                    //    air1.Tag, air2.Tag, air1.TimeStamp);
-                    //Console.SetOut(tw);
-                    //
-                    //sw.Close();
-                    //fs.Close();
-
-                    StreamWriter log;
-                    string writeToLog =
-                        string.Format("WARNING!!!! {0}, you are on a collision course with {1}. At: {2}. Divert course!",
-                        air1.Tag, air2.Tag, air1.TimeStamp);
-                    
-                    string textInFile = File.ReadAllText(@"SeparationEventLogFile.txt");
-
-                    if (!textInFile.Contains(air1.Tag))
-                    {
-                        
-                        if (!File.Exists(@"SeparationEventLogFile.txt"))
-                        {
-                            log = new StreamWriter(@"SeparationEventLogFile.txt");
-                        }
-                        else
-                        {
-                            log = File.AppendText(@"SeparationEventLogFile.txt");
-                        }
-
-                        log.WriteLine(writeToLog);
-                        log.Close();
-                    }
-                }
+                log.WriteLine("Collision event; {0}; {1}; {2}", e.a1.Tag, e.a2.Tag, e.a1.TimeStamp);
             }
-
-            
         }
-        
     }
 }
