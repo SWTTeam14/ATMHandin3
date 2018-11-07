698eccba4b021c1b37b03ddc4a0c465f92ebe149
Anne Porsgaard <anne@besenbacher.com>
2018-11-07 15:40:31 +0100

    Made to tests in Decoder

--- ATMHandin3/Transponder.Test.Unit/DecoderTest.cs
+++ ATMHandin3/Transponder.Test.Unit/DecoderTest.cs
@@ -12,7 +12,7 @@ namespace Transponder.Test.Unit
         private ITransponderReceiver _fakeTransponderReceiver;
         private Decoder _uut;
         private IDecoder _fakeDecoder;
-
+        private int _nDataDecodedEvents = 0;
 
         [SetUp]
         public void Setup()
@@ -21,7 +21,21 @@ namespace Transponder.Test.Unit
             _fakeDecoder = Substitute.For<IDecoder>();
             _uut = new Decoder(_fakeTransponderReceiver);
 
-            _fakeTransponderReceiver.TransponderDataReady += (o, args) => { };
+            _uut.DataDecodedEvent += (o, args) => { ++_nDataDecodedEvents; };
+        }
+
+        [Test]
+        public void TestReception()
+        {
+            List<string> testData = new List<string>();
+            testData.Add("ATR423;39045;12932;14000;20151006213456789");
+            testData.Add("BCD123;10005;85890;12000;20151006213456789");
+            testData.Add("XYZ987;25059;75654;4000;20151006213456789");
+
+            _fakeTransponderReceiver.TransponderDataReady +=
+                Raise.EventWith(_fakeTransponderReceiver, new RawTransponderDataEventArgs(testData));
+
+            Assert.AreEqual(_nDataDecodedEvents, 1);
         }
 
         [Test]
