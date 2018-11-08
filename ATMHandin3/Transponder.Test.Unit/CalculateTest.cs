b2bcf86cb2dac89ea1034b981648305705b7ad6c
Anne Porsgaard <anne@besenbacher.com>
2018-11-08 10:20:21 +0100

    Added a test for calculating the angle

--- ATMHandin3/Transponder.Test.Unit/CalculateTest.cs
+++ ATMHandin3/Transponder.Test.Unit/CalculateTest.cs
@@ -21,7 +21,15 @@ namespace Transponder.Test.Unit
             _tmpTime2 = new DateTime(2018, 07, 09, 20, 40, 20, 902);
 
             Assert.That(Calculate.CalculateVelocity(a, b, c, d, _tmpTime1, _tmpTime2), Is.EqualTo(538.0));
+        }
 
+        [TestCase(0, 2, 0, 1, 63.4349488)]
+        [TestCase(0, 2, 0, -1, 180 - 63.4349488)]
+        [TestCase(0, -2, 0, -1, 180 + 63.4349488)]
+        [TestCase(0, -2, 0, 1, (180 - 63.4349488) + 180)]
+        public void TestCalculateAngle(double x1, double x2, double y1, double y2, double angle)
+        {
+            Assert.That(Calculate.CalculateAngle(x1, x2, y1, y2), Is.InRange(angle - 0.1, angle + 0.1));
         }
     }
 }
