c6123644233d7e14f59434f3e63714d7e4054129
Anne Porsgaard <anne@besenbacher.com>
2018-11-08 10:15:41 +0100

    Made a test for calculating velocity

--- ATMHandin3/Transponder.Test.Unit/CalculateTest.cs
+++ ATMHandin3/Transponder.Test.Unit/CalculateTest.cs
@@ -11,23 +11,16 @@ namespace Transponder.Test.Unit
 {
     public class CalculateTest
     {
-        private DateTime tmpTime1;
-        private DateTime tmpTime2;
-
-        [SetUp]
-
-        public void SetUp()
-        {
-
-        }
+        private DateTime _tmpTime1;
+        private DateTime _tmpTime2;
 
         [TestCase(14000, 12000, 57000, 52000)]
         public void TestCalculateVelocity(int a, int b, int c, int d)
         {
-            tmpTime1 = new DateTime(2018, 07, 09, 20, 40, 10, 902);
-            tmpTime2 = new DateTime(2018, 07, 09, 20, 40, 20, 902);
+            _tmpTime1 = new DateTime(2018, 07, 09, 20, 40, 10, 902);
+            _tmpTime2 = new DateTime(2018, 07, 09, 20, 40, 20, 902);
 
-            //Assert.That(_uut.CalculateVelocity(a, b, c, d, tmpTime1, tmpTime2), Is.EqualTo(538.0));
+            Assert.That(Calculate.CalculateVelocity(a, b, c, d, _tmpTime1, _tmpTime2), Is.EqualTo(538.0));
 
         }
     }
