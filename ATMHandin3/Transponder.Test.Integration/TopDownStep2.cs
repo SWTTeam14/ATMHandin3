57566f0f26ec2c2071cef816c74179b800632d8d
RasmusChristensen <rasmus@christensen.io>
2018-11-10 23:07:26 +0100

    Changed the integration test for separation events, to something way simpler...

--- ATMHandin3/Transponder.Test.Integration/TopDownStep2.cs
+++ ATMHandin3/Transponder.Test.Integration/TopDownStep2.cs
@@ -41,27 +41,17 @@ namespace Transponder.Test.Integration
         public void TestSeparationEventIsRaised()
         {
             _nSeparationEvents = 0;
- 
-            List<Aircraft> testList = new List<Aircraft>();
 
-            DateTime dateTime1 = DateTime.ParseExact("20151006213456789", "yyyyMMddHHmmssfff", System.Globalization.CultureInfo.InvariantCulture);
-            Aircraft aircraft1 = new Aircraft("ATR423", 39045, 12932, 14000, dateTime1);
-            DateTime dateTime2 = DateTime.ParseExact("20151006213558789", "yyyyMMddHHmmssfff", System.Globalization.CultureInfo.InvariantCulture);
-            Aircraft aircraft2 = new Aircraft("BTR700", 19100, 23000, 1000, dateTime2);
-            Aircraft aircraft3 = new Aircraft("CTR100", 19100, 23000, 1000, dateTime2);
+            List<string> testData = new List<string>();
+            testData.Add("ATR423;39045;12932;14000;20151006213456789");
+            testData.Add("BCD123;10005;85890;12000;20151006213456789");
+            testData.Add("XYZ987;10005;85890;12000;20151006213456789");
+
+            RawTransponderDataEventArgs arg = new RawTransponderDataEventArgs(testData);
+            _fakeTransponderReceiver.TransponderDataReady += Raise.EventWith(_fakeTransponderReceiver, arg);
             
-            testList.Add(aircraft1);
-            testList.Add(aircraft2);
-            testList.Add(aircraft3);
-
-            DataDecodedEventArgs decodedArg = new DataDecodedEventArgs(testList);
-            _realDecoder.DataDecodedEvent += Raise.EventWith(_realDecoder, decodedArg);
-
-            _realAmsController.DataDecodedEventHandler(_realDecoder, decodedArg);
-
-            _realAvoidanceSystem.CollisionWarning(_realAmsController.filteredAircrafts);
             //We expect based on the previous assert result, that this will result in a separation event.
-            Assert.AreEqual(_nSeparationEvents, 2);
+            Assert.AreEqual(_nSeparationEvents, 1);
         }
 
         [Test]
@@ -69,23 +59,14 @@ namespace Transponder.Test.Integration
         {
             _nSeparationEvents = 0;
 
-            List<Aircraft> testList = new List<Aircraft>();
+            List<string> testData = new List<string>();
+            testData.Add("ATR423;39045;12932;14000;20151006213456789");
+            testData.Add("BCD123;10005;85890;12000;20151006213456789");
+            testData.Add("XYZ987;25059;75654;4000;20151006213456789");
 
-            DateTime dateTime1 = DateTime.ParseExact("20151006213456789", "yyyyMMddHHmmssfff", System.Globalization.CultureInfo.InvariantCulture);
-            Aircraft aircraft1 = new Aircraft("ATR423", 50000, 50000, 9000, dateTime1);
-            Aircraft aircraft2 = new Aircraft("BTR700", 30000, 30000, 5000, dateTime1);
-            Aircraft aircraft3 = new Aircraft("CTR100", 19100, 23000, 1000, dateTime1);
-
-            testList.Add(aircraft1);
-            testList.Add(aircraft2);
-            testList.Add(aircraft3);
-
-            DataDecodedEventArgs decodedArg = new DataDecodedEventArgs(testList);
-            _realDecoder.DataDecodedEvent += Raise.EventWith(_realDecoder, decodedArg);
-
-            _realAmsController.DataDecodedEventHandler(_realDecoder, decodedArg);
-
-            _realAvoidanceSystem.CollisionWarning(_realAmsController.filteredAircrafts);
+            RawTransponderDataEventArgs arg = new RawTransponderDataEventArgs(testData);
+            _fakeTransponderReceiver.TransponderDataReady += Raise.EventWith(_fakeTransponderReceiver, arg);
+            
             //We expect based on the previous assert result, that this will result in a separation event.
             Assert.AreEqual(_nSeparationEvents, 0);
         }
