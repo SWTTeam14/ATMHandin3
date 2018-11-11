374d63a10617be2267379f74b69910db141272fa
andreas <androgles1994@gmail.com>
2018-11-11 21:50:17 +0100

    Made a test for filteredAircraftEvent and made a test where 2 aircrafts enters airspace and 1 leaves. Testing if the events are raised. This is done to test if the AMSController is working with the rest of the already tested system

--- ATMHandin3/Transponder.Test.Integration/TopDownStep1.cs
+++ ATMHandin3/Transponder.Test.Integration/TopDownStep1.cs
@@ -8,6 +8,7 @@ using NUnit.Framework;
 using NSubstitute;
 using TransponderReceiver;
 using ATMHandin3.Classes;
+using ATMHandin3.Events;
 
 namespace Transponder.Test.Integration
 {
@@ -20,24 +21,21 @@ namespace Transponder.Test.Integration
         private ATMHandin3.Classes.Decoder decoder;
 
         private AMSController amsController;
-
-
+        
         private int _nFilteredAircraftEvent = 0;
         private int _nTrackEnteredAirspaceEvent = 0;
         private int _nTrackLeftAirspaceEvent = 0;
-
+        
         [SetUp]
         public void SetUp()
         {
-            airspace = new Airspace(10000, 10000, 90000, 90000, 500, 20000);
-
-            amsController = new AMSController(decoder, airspace);
-            decoder = new Decoder(transponder);
-
-
+            
             iCol = Substitute.For<ICollisionAvoidanceSystem>();
             transponder = Substitute.For<ITransponderReceiver>();
-
+            
+            airspace = new Airspace(10000, 10000, 90000, 90000, 500, 20000);
+            decoder = new Decoder(transponder);
+            amsController = new AMSController(decoder, airspace);
 
             amsController.FilteredAircraftsEvent += (o, args) => { ++_nFilteredAircraftEvent; };
             amsController.TrackEnteredAirspaceEvent += (o, args) => { ++_nTrackEnteredAirspaceEvent; };
@@ -45,15 +43,52 @@ namespace Transponder.Test.Integration
         }
 
         [Test]
-        public void Test_seperation_event_correct_count()
+        public void test_that_a_filteredAircraftEvent_is_raised_when_transponderData_is_ready()
+        {
+            string testData1 = "ATR423;39044;12931;13999;20151006213456789";
+            string testData2 = "GFD123;39045;12932;14000;20151006213456789";
+            string testData3 = "MKD936;90001;12932;14000;20151006213456789";
+
+            List<string> aircraftTestData = new List<string>();
+            aircraftTestData.Add(testData1);
+            aircraftTestData.Add(testData2);
+            aircraftTestData.Add(testData3);
+
+            transponder.TransponderDataReady += Raise.EventWith(this, new RawTransponderDataEventArgs(aircraftTestData));
+            
+            Assert.That(_nFilteredAircraftEvent, Is.EqualTo(1));
+           
+        }
+
+
+        [Test]
+        public void test_2_tracks_enter_airspace_and_1_leave()
         {
             string testData1 = "ATR423;39045;12932;14000;20151006213456789";
             string testData2 = "GFD123;39045;12932;14000;20151006213456789";
-            string testData3 = "MKD936;39045;12932;14000;20151006213456789";
+            string testData3 = "MKD936;90001;12932;14000;20151006213456789";
 
-            List<string>
-            transponder.TransponderDataReady += Raise.EventWith(this,);
+            List<string> aircraftTestData = new List<string>();
+            aircraftTestData.Add(testData1);
+            aircraftTestData.Add(testData2);
+            aircraftTestData.Add(testData3);
+
+            transponder.TransponderDataReady += Raise.EventWith(this, new RawTransponderDataEventArgs(aircraftTestData));
+
+            Assert.That(_nTrackEnteredAirspaceEvent, Is.EqualTo(2));
+
+            testData1 = "ATR423;40001;12932;14000;20151006213456789";
+            testData2 = "GFD123;90001;12932;14000;20151006213456789";
+            testData3 = "MKD936;90003;12932;14000;20151006213456789"; // this one never been inside
+            
+            aircraftTestData.Add(testData1);
+            aircraftTestData.Add(testData2);
+            aircraftTestData.Add(testData3);
+
+            transponder.TransponderDataReady += Raise.EventWith(this, new RawTransponderDataEventArgs(aircraftTestData));
+
+            Assert.That(_nTrackLeftAirspaceEvent, Is.EqualTo(1));
 
         }
-}
+    }
 }
